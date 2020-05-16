dc0f075898de4a75d97ecf75a2960474ddce7426
DungDemo <dungdemoictu001@gmail.com>
2020-05-16 16:28:09 +0700

    9/5/2020

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -32,6 +32,17 @@ class Controller extends BaseController
 
     public function index()
     {
-            return "index";
+        $cart = Cart::where("user_id", 2)->with(["detail" => function ($query) {
+            $query->join("product","product.product_id","=","cart_detail.product_id")->whereRaw("product.amount-quantity >= 0 ");
+        }])->get()->first();
+        foreach ($cart->detail as $key => $data) {
+               $data->delete();
+        }
+        $cart->fresh();
+        echo $cart->count();
+        if($cart->count()<0){
+            $cart->delete();
+        }
+       return response()->json($cart, 200);
     }
 }
